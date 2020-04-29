9e11a6caf020af4b1584fc75dfc4683db225cfa1
ogunduz1 <64080312+ogunduz1@users.noreply.github.com>
2020-04-29 18:48:51 +0300

    fonksiyon1.java
    basit fonksiyon örnekleri

--- fonksiyon1.java
+++ fonksiyon1.java
@@ -1,11 +1,23 @@
 public class fonksiyon1 {
 
-	//f(x)=5x+10 fonksiyonun kod uygulaması
+	/*fonksiyon bir değer geri getirmek veya parametrik olmak zorunda değil
+	 * aşağıdaki ilk fonksiyon bunun örneğidir.
+	 * void geri dönüş olmayacağı manasına gelir*/
+	static void yaz() {
+		System.out.println("Merhaba fonksiyon");
+	}
+	//f(x,y)=5x+10 fonksiyonun kod uygulaması
 	static int f(int x) {
 		return (5*x+10);
 	}
-	
+	/*bir fonkisyonun içine başka fonsiyon sokulup işlem yapılabilir,
+	 * matematikteki f o g(x) 
+	 * aşağıdaki fonksiyon bu duruma örnektir*/ 
+	static int g(int x,int y) {
+		return 	f(y+2*x);
+	}
 	public static void main(String[] args) {
-		System.out.println(f(5));
+		System.out.println(g(5,3));
+		yaz();
 	}
 }
