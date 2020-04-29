834c557a31efffc124f5733b696a59baaaf84add
ogunduz1 <64080312+ogunduz1@users.noreply.github.com>
2020-04-29 18:34:22 +0300

    fonksiyon1.java
    çok basit seviyede fonksiyon örneği

---
+++ fonksiyon1.java
@@ -0,0 +1,11 @@
+public class fonksiyon1 {
+
+	//f(x)=5x+10 fonksiyonun kod uygulaması
+	static int f(int x) {
+		return (5*x+10);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(f(5));
+	}
+}
