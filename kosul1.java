26d6197f8984c46fd9ffcb8239b289877431cd64
ogunduz1 <64080312+ogunduz1@users.noreply.github.com>
2020-04-29 15:54:17 +0300

    kosul1.java
    if statement için ilk örnek

---
+++ kosul1.java
@@ -0,0 +1,15 @@
+public class kosul1 {
+	public static void main(String[] args) {
+		int kosul=1;
+		if(kosul==10) {
+			System.out.println("Kosulun değeri 10 dur");
+		}else if (kosul<10) {
+			System.out.println("kosulun değeri 10dan küçüktür");
+		}else {
+			System.out.println("kosulun değeri 10dan büyüktür");
+		}
+		System.out.println("kosul dışı kontrolsüz");
+	
+		
+	}
+}
